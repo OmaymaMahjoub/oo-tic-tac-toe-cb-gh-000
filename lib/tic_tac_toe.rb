5c9659a7ed29c9120d4ec6baa14665898a2b5816
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-07 17:35:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,24 +2,24 @@ class Tic_Tac_Toe
     def initialize(board = nil)
       @board = board || Array.new(9, " ")
     end
-    def display_board(board=[" "," "," "," "," "," "," "," "," "])
-      puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
       puts "-----------"
-      puts " #{board[3]} | #{board[4]} | #{board[5]} "
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
       puts "-----------"
-      puts " #{board[6]} | #{board[7]} | #{board[8]} "
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
 
     def input_to_index(input)
       return input.to_i-1
     end
 
-    def position_taken?(board=[" "," "," "," "," "," "," "," "," "],index=0)
-      return (board[index]=="X")||(board[index]=="O")
+    def position_taken?(index=0)
+      return (@board[index]=="X")||(@board[index]=="O")
     end
 
-    def valid_move?(board=[" "," "," "," "," "," "," "," "," "],index=0)
-      if position_taken?(board,index)
+    def valid_move?(index=0)
+      if position_taken?(index)
         return false
       elsif (index>8) || (index<0)
         return false
@@ -27,18 +27,18 @@ class Tic_Tac_Toe
       return true
     end
 
-    def move(board,index,input)
-      if valid_move?(board,index)
-        board[index]=input
+    def move(index,input)
+      if valid_move?(index)
+        @board[index]=input
         return true
       else
         return false
       end
     end
 
-    def turn_count(board=[" "," "," "," "," "," "," "," "," "])
+    def turn_count
       counter=0
-      board.each do |input|
+      @board.each do |input|
         if (input=="X")||(input=="O")
           counter+=1
         end
@@ -46,69 +46,69 @@ class Tic_Tac_Toe
       return counter
     end
 
-    def current_player(board)
-      counter=turn_count(board)
+    def current_player
+      counter=turn_count()
       if counter%2==0
         return "X"
       end
       return "O"
     end
 
-    def turn (board=[" "," "," "," "," "," "," "," "," "])
+    def turn 
       puts "Please enter 1-9:"
       input=gets.strip
       index=input_to_index(input)
-      input=current_player(board)
-      test=move(board,index,input)
+      input=current_player()
+      test=move(index,input)
       until test
         puts "Please enter 1-9:"
         input=gets.strip
         index=input_to_index(input)
-        input=current_player(board)
-        test=move(board,index,input)
+        input=current_player()
+        test=move(index,input)
       end
       display_board(board)
     end
 
     WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]]
 
-    def won?(board)
+    def won?
       WIN_COMBINATIONS.each do |win_combination|
-        if (board[win_combination[0]]==board[win_combination[1]])&&(board[win_combination[1]]==board[win_combination[2]])&&((board[win_combination[1]]=="X")||(board[win_combination[1]]=="O"))
+        if (@board[win_combination[0]]==@board[win_combination[1]])&&(@board[win_combination[1]]==@board[win_combination[2]])&&((@board[win_combination[1]]=="X")||(@board[win_combination[1]]=="O"))
           return win_combination
         end
       end
       return false
     end
 
-    def full?(board)
-      return !(board.include?(" "))
+    def full?
+      return !(@board.include?(" "))
     end
 
-    def draw?(board)
-      return (full?(board)&&!won?(board))
+    def draw?
+      return (full?()&&!won?())
     end
 
-    def over?(board)
-      return (draw?(board)||won?(board))
+    def over?
+      return (draw?()||won?())
     end
 
-    def winner(board)
-      arr=won?(board)
+    def winner
+      arr=won?()
       if arr!=false
-        return board[arr[0]]
+        return @board[arr[0]]
       end
       return nil
     end
 
     def play()
-      while !over?(@board)
-        turn(@board)
+      while !over?()
+        turn()
       end
-      if draw?(@board)
+      if draw?()
         puts"Cat's Game!"
       else
-        input=winner(@board)
+        input=winner()
         puts "Congratulations #{input}!"
       end
     end
